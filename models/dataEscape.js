699c9fb94fc26de21c412d36ec1b93111e3ccbb5
silvakwan1 <kauans003@gmail.com>
2024-10-06 19:30:39 -0300

    I changed the folder name and added one more attribute to the file.

---
+++ models/dataEscape.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const dataEscapeSchema = new mongoose.Schema({
+  paragraph: {
+    type: String,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  lestUrl: {
+    type: String,
+    required: true,
+  },
+  imgUrl: {
+    type: String,
+    required: true,
+  },
+});
+
+const DataEscape = mongoose.model("DataEscape", dataEscapeSchema);
+
+module.exports = DataEscape;
