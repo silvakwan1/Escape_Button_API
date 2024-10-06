e012f1f80255d76c040aa3ebc0ef70d0409e9cb3
silvakwan1 <kauans003@gmail.com>
2024-10-06 17:37:07 -0300

    Create a POST route to create data.

---
+++ routes/craeteDataEscape.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const DataEscape = require("../config/dataEscape");
+
+router.post("/", async (req, res, next) => {
+  const { text, lestUrl, imgUrl } = req.body;
+
+  if (!text) return res.status(400).json({ mensagem: "not fold tetx" });
+  if (!lestUrl) return res.status(400).json({ mensagem: "not fold lest url" });
+  if (!imgUrl) return res.status(400).json({ mensagem: "not fold img url" });
+
+  try {
+    const dateBtnEscape = new DataEscape({
+      text,
+      lestUrl,
+      imgUrl,
+    });
+
+    const resBtnEscape = await dateBtnEscape.save();
+
+    return res.status(201).json(resBtnEscape);
+  } catch (error) {
+    console.error;
+  }
+});
+
+module.exports = router;
