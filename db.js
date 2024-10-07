4a18d4c070f19c6747794713b1ce88815fef6962
silvakwan1 <kauans003@gmail.com>
2024-10-06 23:38:32 -0300

    updade

--- db.js
+++ db.js
@@ -1,14 +1,9 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const dbUser = process.env.DB_USER;
-const DB_PASS;
+const url = process.env.MONGODB_URI;
+
 const dbName = "EscapeButtonAPI";
-
-const url = process.env.MONGODB_URI
-
-`mongodb+srv://${dbUser}:${dbPass}@cluster0.q0hds.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`;
-
 async function connectToDatabase() {
   try {
     await mongoose.connect(url, {
