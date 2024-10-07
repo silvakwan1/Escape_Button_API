c2e80fe1cc69239419d9f709e210a014d40a3742
silvakwan1 <kauans003@gmail.com>
2024-10-07 01:09:41 -0300

    update

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.render("index", { title: "Express 0000" });
+  res.json({ msn: "rodando" });
 });
 
 module.exports = router;
